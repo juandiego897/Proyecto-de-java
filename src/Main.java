74a7d1fa03f8129f42f0b29eaad529423c77b29a
juandiego897 <jd.jaramillo23@ciaf.edu.co>
2024-03-08 18:41:28 -0500

    update

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a year, only numbers please:");
+        int year = scanner.nextInt();
+
+        if (isleap(year)) {
+            System.out.println(year + " this year is leap year");
+        } else {
+            System.out.println(year + " this year is not a leap year.");
+        }
+    }
+
+    public static boolean isleap(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            return true;
+        }
+        return false;
+    }
+}
